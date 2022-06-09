d87e04201fda6a1dc6d4261fd33da88f500961ff
Ndunge-Makau <winniemakau@outlook.com>
2022-06-09 05:56:54 -0700

    Correct 4-puts.c.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-/*
- * main - print the string in the puts function
- * Description: prints "Programming is like building a multilingual puzzle
- * Return: 0
-*/
+
 int main(void)
 {
-puts("\"Programming is like a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
