8de09bc7d70804c4dec0409e048cc64ad6e662e3
Ndunge-Makau <winniemakau@outlook.com>
2022-06-09 08:53:33 -0700

    Add 6-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: prints the size of various types
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	int long c;
+	int long long d;
+	float f;
+
+	printf("Size of a char: %zu bytes \n", sizeof(a));
+	printf("Size of an int: %zu bytes \n", sizeof(b));
+	printf("Size of a long int: %zu bytes \n", sizeof(c));
+	printf("Size of a long long int: %zu bytes \n", sizeof(d));
+	printf("Size of a float: %zu bytes \n", sizeof(f));
+	return (0);
+}
