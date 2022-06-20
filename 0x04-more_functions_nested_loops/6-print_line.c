adaa5e2fb0b8a1e8131095d8b3c52b324abf5e75
Ndunge-Makau <winniemakau@outlook.com>
2022-06-20 05:54:42 -0700

    Add 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of count
+ * Description: draws a straight line in the terminal
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
