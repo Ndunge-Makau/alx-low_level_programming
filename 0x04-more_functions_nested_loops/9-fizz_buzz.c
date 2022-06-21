d8310d790d97ba8e35b9554ebe07a2cb43075be0
Ndunge-Makau <winniemakau@outlook.com>
2022-06-21 00:55:41 -0700

    Add 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz
+ * Description: prints the numbers from 1 to 100, FizzBuzz format
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3) == 0)
+		{
+			if ((a % 5) == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+				printf("Fizz ");
+			}
+		}
+		else if ((a % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
