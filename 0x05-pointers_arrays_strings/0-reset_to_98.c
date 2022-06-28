72a76cb3da1a54c92de74c305897a63e77cf25df
Ndunge-Makau <winniemakau@outlook.com>
2022-06-28 05:45:36 -0700

    Add 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update value to 98
+ * @n: the integer to be checked
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains solutions for the 0x05. C - Pointers, arrays and strings assignment.
