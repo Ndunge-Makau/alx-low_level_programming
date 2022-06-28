4efe422d3108d703cad651c0d59f0dc6301461e8
Ndunge-Makau <winniemakau@outlook.com>
2022-06-28 07:48:02 -0700

    Add 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int length = 0;
+	int n;
+
+	while (str[i++])
+		length++;
+	if ((length % 2) == 0)
+	{
+		n = length / 2;
+	}
+	else
+	{
+		n = (length + 1) / 2;
+	}
+	for (i = n; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
