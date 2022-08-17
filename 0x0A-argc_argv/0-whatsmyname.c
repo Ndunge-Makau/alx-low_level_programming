c00711137024636bd6d665c2f5e85483afb378b4
Ndunge-Makau <winniemakau@outlook.com>
2022-08-17 02:21:58 -0700

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print the name of program
+ * @argc: sixe parameter
+ * @argv: Array containing the program
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
