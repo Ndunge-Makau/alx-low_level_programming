18bb3a71e80d60f6bb93db97cc5bbb9d39038464
Ndunge-Makau <https://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@github.com/Ndunge-Makau/alx-low_level_programming.git>
2022-08-19 01:09:15 -0700

    Correct 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,14 +5,16 @@
  * main - adds positive numbers
  * @argc: number of arguments
  * @argv: array containing arguments
- * Return: 1 f one of the number contains 
- * 	symbols that are not digits,
- * 	0 otherwise
+ * Return: 1 if one of the number contains
+ * symbols that are not digits,
+ * 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-	int i, x, sum = 0;
-	
+	int i, x;
+	int sum = 0;
+
 	for (i = 1; i < argc; i++)
 	{
 		for (x = 0; argv[i][x]; x++)
@@ -22,11 +24,8 @@ int main(int argc, char *argv[])
 				printf("Error\n");
 				return (1);
 			}
-			else
-			{
-				sum += atoi(argv[i]);
-			}
 		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
