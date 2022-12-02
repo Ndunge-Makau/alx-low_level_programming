13c9edb1b9fadd4d7765e1bd314b1a26579c356e
Ndunge-Makau <105147786+Ndunge-Makau@users.noreply.github.com>
2022-12-02 11:23:08 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
- * Description: prints the size of various types
- * Return: 0
+ * main: Entry Point
+ * Description: Print size of variable types
+ * Return: 0 
  */
 
 int main(void)
 {
-	char a;
-
-	int b;
-
-	int long c;
-
-	int long long d;
-
-	float f;
-
-	printf("Size of a char: %zu byte(s)\n", sizeof(a));
-	printf("Size of an int: %zu byte(s)\n", sizeof(b));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-	printf("Size of a float: %zu byte(s)\n", sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
