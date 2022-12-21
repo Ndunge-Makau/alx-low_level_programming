deba60288c92434f057a9c7837e25e5dde9dbcc6
Ndunge-Makau <winniemakau@outlook.com>
2022-12-21 01:33:08 -0800

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,15 +10,19 @@
 
 void print_array(int *a, int n)
 {
-	int i;
+	int i = 0;
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 
-		if (i == n - 1)
-			continue;
-		printf(", ");
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
 	}
-	printf("\n");
 }
