ee7a9658540ab024bd04bee75a1985282518342c
Ndunge-Makau <winniemakau@outlook.com>
2023-06-15 00:25:07 -0700

    Correct 2-str_concat.c.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, length1 = 0, length2 = 0;
 	char *new_string;
 
-	if (s1 == NULL || s2 == NULL)
+	if ((s1 == NULL || s2 == NULL) || (s1 == NULL && s2 == NULL))
 	{
 		return (NULL);
 	}
