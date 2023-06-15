ceea79f1b83a30239e3a6e0b2a95e5e72cc21e75
Ndunge-Makau <winniemakau@outlook.com>
2023-06-15 00:35:03 -0700

    Correct 2-str_concat.c.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, length1 = 0, length2 = 0;
 	char *new_string;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	else if (s1 == NULL && s2 != NULL)
-		return (s2);
-	else if (s1 != NULL && s2 == NULL)
-		return (s1);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[i])
 	{
