00fdb309d687854d76547c08ba8b26fe4b0f0840
Ndunge-Makau <winniemakau@outlook.com>
2023-08-15 04:10:17 -0700

    Edit 4-new_dog.c and dog.h.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -52,9 +52,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *New_dog;
 
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-
 	New_dog = malloc(sizeof(dog_t));
 
 	if (New_dog == NULL)
