b48f304e879b70047e0b1b03ca7e4d178f2657c7
Ndunge-Makau <winniemakau@outlook.com>
2023-08-15 01:26:48 -0700

    Add dog.h.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - Describes a dog.
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* #ifndef dog */
