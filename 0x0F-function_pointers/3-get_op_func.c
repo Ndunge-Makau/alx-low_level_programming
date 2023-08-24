fc91a8cade26c78877d5fdc4e0e048ce504da75c
Ndunge-Makau <winniemakau@outlook.com>
2023-08-24 03:37:14 -0700

    Fix 3-get_op_func.c.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,12 +18,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 5; i++)
+	while (i < 5)
 	{
 		if (ops[i].op[0] == *s)
 			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
