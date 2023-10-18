5b03465d17501e292289f2e5c01a88053a5bd2e5
Ndunge-Makau <winniemakau@outlook.com>
2023-10-18 03:23:19 -0700

    Update 0-print_list.c and 2-add_node.c.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <unistd.h>
 
 /**
  * print_list - prints all the elements of a list_t list.
@@ -9,16 +10,40 @@
 size_t print_list(const list_t *h)
 {
 	size_t num = 0;
+	int i = 0;
 	const list_t *temp = h;
+	char *my_string;
 
 	while (temp != NULL)
 	{
 		if (temp->string == NULL)
-			printf("[0] (nil)\n");
+			my_string = "[0] (nil)";
 		else
-			printf("[%d] %s\n", temp->len, temp->string);
+			my_string = temp->string;
+		_putchar('[');
+		_putchar('0' + temp->len);
+		_putchar(']');
+		_putchar(' ');
+		while (my_string[i])
+		{
+			_putchar(my_string[i]);
+			i++;
+		}
+		i = 0;
+		_putchar('\n');
 		num++;
 		temp = temp->next;
 	}
 	return (num);
+}
+
+/**
+ * _putchar - print character
+ * @c: character
+ * Return: 1 on success, -1 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
