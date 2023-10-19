f665194a3cb4d1af6fea2e81204af356c788c792
Ndunge-Makau <winniemakau@outlook.com>
2023-10-19 00:32:57 -0700

    Update 0-print_list.c.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <unistd.h>
 
 /**
  * print_list - prints all the elements of a list_t list.
@@ -9,47 +8,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t num = 0, length = 0;
-	int i = 0;
+	size_t num = 0;
 	const list_t *temp = h;
-	char *my_string;
 
 	while (temp != NULL)
 	{
 		if (temp->str == NULL)
-		{
-			length = 0;
-			my_string = "(nil)";
-		}
+			printf("[0] (nil)\n");
 		else
-		{
-			length = temp->len;
-			my_string = temp->str;
-		}
-		_putchar('[');
-		_putchar('0' + length);
-		_putchar(']');
-		_putchar(' ');
-		while (my_string[i])
-		{
-			_putchar(my_string[i]);
-			i++;
-		}
-		i = 0;
-		_putchar('\n');
+			printf("[%d] %s\n", temp->len, temp->str);
 		num++;
 		temp = temp->next;
 	}
 	return (num);
-}
-
-/**
- * _putchar - print character
- * @c: character
- * Return: 1 on success, -1 on error
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
