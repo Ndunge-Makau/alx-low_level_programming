f7307225a1e2543ed27d2df83d9dd7268fbf349f
Ndunge-Makau <winniemakau@outlook.com>
2023-12-22 01:01:47 -0800

    Add 0-print_listint.c.

---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This folder contains solutions for the  0x13-more_singly_linked_lists assignment.
---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * _putchar - prints character
+ * @c: character
+ * Return: int
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
