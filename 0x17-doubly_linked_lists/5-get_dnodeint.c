6c0c8c8cb92006ae10826d58137e4b5a227f28ff
Ndunge-Makau <winniemakau@outlook.com>
2024-02-12 02:13:08 -0800

    Fix 5-get_dnodeint.c.

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 	}
 	temp = head;
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index && temp; i++)
 	{
 		temp = temp->next;
 	}
