ccccb77d3a3739c51fb2f638fed527e2745bc36d
Ndunge-Makau <winniemakau@outlook.com>
2024-02-12 00:59:56 -0800

    Add 4-free_dlistint.c.

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,23 +1,19 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * free_dlistint - function that frees a dlistint_t list
- * @head: doubly list to free
- * Return: No return.
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: Start of list
+ * Return: Nothing
  */
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp = NULL;
+	dlistint_t *temp;
 
 	while (head)
 	{
-		tmp = head;
-		head = head->next;
-		free(tmp);
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
