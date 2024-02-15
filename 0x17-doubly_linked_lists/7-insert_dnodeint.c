6f448e85b315d9ab2d6c7e5dc4b04a690310e0e4
Ndunge-Makau <winniemakau@outlook.com>
2024-02-15 00:24:30 -0800

    Try fix .

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,6 +11,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h, *temp2, *new;
+	unsigned int count = 0;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL || *h == NULL)
@@ -19,25 +20,41 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new->n = n;
-	if (idx == 0)
-		new = add_dnodeint(h, n);
-	while (temp->next != NULL && idx != 1)
+	while (temp)
 	{
+		count++;
 		temp = temp->next;
-		idx--;
 	}
-	if (temp->next == NULL)
+	if (count < idx - 1)
 	{
-		temp->next = new;
-		new->prev = temp;
+		free(new);
+		return (NULL);
+	}
+	else if (count == idx - 1)
+	{
+		new = add_dnodeint_end(h, n);
+		return (new);
 	}
 	else
 	{
-		temp2 = temp->next;
-		temp->next = new;
-		temp2->prev = new;
-		new->next = temp2;
-		new->prev = temp;
+		if (idx == 0)
+			new = add_dnodeint(h, n);
+		else
+		{
+			temp = *h;
+			count = 0;
+			while (count < idx - 1)
+			{
+				temp = temp->next;
+				count++;
+			}
+			temp2 = temp->next;
+			new->next = temp2;
+			new->prev = temp;
+			temp2->prev = new;
+			temp->next = new;
+		}
 	}
 	return (new);
 }
+
